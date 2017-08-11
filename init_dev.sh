5574a8871251c045bf4cdd69ed9a7b97bc9ec62a
xpure <xpure@foxmail.com>
2017-08-11 22:56:18 +0800

    f

--- init_dev.sh
+++ init_dev.sh
@@ -3,17 +3,19 @@
 PREFIX=`realpath $(cd "$(dirname "$0")"; pwd)`
 cd ~
 
-if [ ! -f "~/.py3env/bin/activate" ]; then
-virtualenv -p /usr/bin/python3 ~/.py3env
-fi
 
-function copy {
-    rm ~/$1
+function home_link {
+    rm -rf ~/$1
     ln -s $PREFIX/home/$1 ~
 }
 
 
-copy .hgrc
-copy .bashrc
-copy .hgignore
+home_link .hgrc
+home_link .tmux_default
+home_link .bashrc
+home_link .hgignore
+home_link .pip
 
+if [ ! -f "~/.py3env/bin/activate" ]; then
+virtualenv -p /usr/bin/python3 ~/.py3env
+fi
