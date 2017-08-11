115646ac9c846c74029250e762d7170e857ce949
xpure <xpure@foxmail.com>
2017-08-11 22:39:56 +0800

    f

--- init_dev.sh
+++ init_dev.sh
@@ -3,11 +3,15 @@
 PREFIX=`realpath $(cd "$(dirname "$0")"; pwd)`
 cd ~
 
+virtualenv -p /usr/bin/python3.6 ~/.py3env
+
 function copy {
     rm ~/$1
     ln -s $PREFIX/home/$1 ~
 }
 
+
 copy .hgrc
 copy .bashrc
 copy .hgignore
+
